753010645e990e17f0604c9325a42fd853593c2e
tvarley <tim.varley@gmail.com>
2015-01-13 16:18:39 -0500

    Solution 7

--- src/euler007.cpp
+++ src/euler007.cpp
@@ -5,85 +5,113 @@
 //
 // What is the 10001st prime number?
 
+// Answer: 104743
+
 #include <iostream>
+#include <vector>
+
+#include <cmath>
 
 using namespace std;
 
-class Cbool
+// Input: an integer n > 1
+//
+// Let A be an array of Boolean values, indexed by integers 2 to n,
+// initially all set to true.
+//
+// for i = 2, 3, 4, ..., not exceeding √n:
+// if A[i] is true:
+// for j = i2, i2+i, i2+2i, ..., not exceeding n :
+// A[j] := false
+//
+// Output: all i such that A[i] is true.
+
+// @see http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+class CSieveOfEratosthenes
 {
-  bool m_value;
 public:
-  Cbool()
+  CSieveOfEratosthenes(int a_upper = 0) : m_upper(a_upper), m_primes(nullptr)
   {
-    m_value = false;
+    init();
   }
 
-  bool const& operator()()const
+  virtual ~CSieveOfEratosthenes()
   {
-    return m_value;
+
   }
 
-  void operator()(bool const& a_new)
+  int get_nth(int a_pos)
   {
-    m_value = a_new;
-  }
-};
-
-class CSieve
-{
-  Cbool* [] m_flags;
-  size_t  m_num_flags;
-
-public:
-  explicit CSieve(size_t num_flags) : m_num_flags(num_flags)
-  {
-    m_flags = new Cboo[num_flags];
-    for (size_t i = 0;  i < m_num_flags; ++i) {
-      mark_multiple(i+1);
-    }
-  }
-
-  void mark_multiple( uint32_t mark_me )
-  {
-    if( mark_me >= 2 )
-    {
-      // cout << "Mark: " << mark_me << endl;
-      uint32_t i = 1;
-      uint32_t idx;
-      while ( (idx = (mark_me*i)) <= m_num_flags) {
-        // cout << "Idx: " << idx << endl;
-        m_flags[idx-1] = true;
-        ++i;
+    int value = 0;
+    int count = a_pos;
+    size_t i = 0;
+    for ( i = 0; i < m_primes->size() && a_pos ; i++) {
+      // cout << "O: " << i << endl;
+      if( true == (*m_primes)[i] ){
+        // cout << "    **" << endl;
+        value = i;
+        a_pos--;
       }
     }
-    return;
+
+    if( a_pos == 0 ){
+      return value;
+    }else{
+      return 0;
+    }
   }
 
   void dump(void)
   {
-    if( null != m_flags ){
-      for (size_t i = 0;  < m_num_flags; ++i) {
-        if( m_flags[i]){
-          cout << "Prime: " << i << endl;
+    cout << "Sieve dump" << endl;
+    cout << "Upper: " << m_upper << endl;
+
+    for (size_t i = 0; i < m_primes->size() ; i++) {
+      if( true == (*m_primes)[i] ){
+        cout << i << ',';
+      }
+    }
+    cout << endl;
+  }
+
+protected:
+  void init()
+  {
+    delete [] m_primes;
+
+    if( m_upper <= 0 )
+    {
+      return;
+    }
+
+    int sqrtupper = (int)floor(sqrt(m_upper));
+
+    m_primes = new vector<bool>(m_upper,true);
+
+    (*m_primes)[0] = false;
+    (*m_primes)[1] = false;
+
+    for (size_t i = 2; i <= sqrtupper ; i++ ) {
+      // cout << "O:" << i << endl;
+      if( true == (*m_primes)[i]){
+        for (size_t j = (i*i); j < m_upper; j += i ) {
+          // cout << "    I:" << j << endl;
+          (*m_primes)[j] = false;
         }
       }
     }
   }
 
-  virtual ~CSieve()
-  {
-    delete [] m_flags;
-  }
-protected:
 private:
-
+  int m_upper;
+  vector<bool>*  m_primes;
 };
-
 
 int main( int argc , char* argv[])
 {
-  auto_ptr<CSieve> cs( new CSieve(10));
-  if( null != cs ){
-    cs->dump();
-  }
+  CSieveOfEratosthenes* sieve = new CSieveOfEratosthenes(110000);
+  sieve->dump();
+
+  int find_me = 10001;
+  cout << "Nth: " << find_me << '=' << sieve->get_nth(find_me) << endl;
 }

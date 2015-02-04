dc6faa70ccc9c993539c685987de34b125bbdf7f
tvarley <tim.varley@gmail.com>
2015-02-04 11:09:13 -0500

    Resurect 002

--- src/test_prob_003.h
+++ src/test_prob_003.h
@@ -1,1 +1,11 @@
+#include "euler003.cpp"
 
+namespace{
+  TEST(Prob003,Known001){
+    EXPECT_EQ(29,largest_prime_factor_brute(13195));
+  }
+
+  TEST(Prob003,Answer){
+    EXPECT_EQ(6857,largest_prime_factor_brute(600851475143));
+  }
+};

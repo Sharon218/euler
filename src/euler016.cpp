40325105218c91da334f55a336c0087fb80cce77
tvarley <tim.varley@gmail.com>
2015-02-12 08:41:09 -0500

    Problem 16 Soution

--- src/euler016.cpp
+++ src/euler016.cpp
@@ -20,24 +20,13 @@ int power_digit_sum(size_t max)
 
   for (size_t i = 0; i < max; i++) {
     int carry = 0;
+
     std::for_each(numbers.begin(),numbers.end(),[&carry](int&  n){
       n *= 2;
-      std::cout << "1) n*=2:" << n << std::endl;
       n += carry;
-      std::cout << "2) n += carry:" << n << std::endl;
-      if( n >= 10 ) carry = 1;
-      std::cout << "3) carry:" << carry << std::endl;
+      carry = ( n >= 10 )?1:0;
       n -= (carry * 10);
-      std::cout << "4) n -= (carry * 10):" << n << std::endl;
     });
-
-    std::cout << i << "----------------" << carry << std::endl;
-
-    for (auto itr = numbers.rbegin() ; itr != numbers.rend() ; itr++) {
-      std::cout << *itr;
-    }
-    std::cout << std::endl << "--------------------"<< std::endl;
-
 
     if( 0 != carry ){
       numbers.push_back(carry);
@@ -56,6 +45,7 @@ int power_digit_sum(size_t max)
 #if ! defined UNITTEST_MODE
 int main(int argc, char const *argv[]) {
   std::cout << "Answer: " << power_digit_sum(15) << std::endl;
+  std::cout << "Answer: " << power_digit_sum(1000) << std::endl;
   return 0;
 }
 #endif // #if ! defined UNITTEST_MODE

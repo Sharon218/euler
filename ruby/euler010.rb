ad178ac8cdaa1a2718a46617e1a9646d0aff3012
tvarley <tim.varley@gmail.com>
2015-12-12 09:57:39 -0500

    010 Ruby solution

---
+++ ruby/euler010.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+#
+# https://projecteuler.net/problem=10
+# Summation of primes
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+#
+# Answer: 142913828922
+require 'prime'
+
+def sum_primes
+  sieve = Prime::EratosthenesGenerator.new
+  prime_sum = 0
+  prime = sieve.next
+  while prime <= 2_000_000
+    prime_sum += prime
+    prime = sieve.next
+  end
+  prime_sum
+end
+
+puts "Answer: #{sum_primes}"

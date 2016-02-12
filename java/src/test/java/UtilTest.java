9c419aba450019814c352da881aa854a543398b4
tvarley <tim.varley@gmail.com>
2016-02-12 07:55:06 -0500

    Java version

--- java/src/test/java/UtilTest.java
+++ java/src/test/java/UtilTest.java
@@ -18,4 +18,8 @@ public class UtilTest {
   @Test public void testPrime02() {
     assertFalse("isPrime should return false",Prime.isPrime(8));
   }
+
+  @Test public void testPrime03() {
+    assertTrue("isPrime should return true",Prime.isPrime(997));
+  }
 }
